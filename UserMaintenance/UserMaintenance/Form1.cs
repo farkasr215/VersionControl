553b0cf253a5a9afc3abdcbf3bb42584a0c12993
farkasr215 <farkasr215@gmail.com>
2019-12-01 20:35:53 +0100

    FirstName and LastName removed, label2 and textBox2 removed, FullName instead

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -7,17 +7,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserMaintenance.Entities;
 
 namespace UserMaintenance
 {
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();
         public Form1()
         {
             InitializeComponent();
-            lblLastName.Text = Resource1.LastName; // label1
-            lblFirstName.Text = Resource1.FirstName; // label2
-            btnAdd.Text = Resource1.Add; // button1
+            label1.Text = Resource1.FullName; // label1
+            button1.Text = Resource1.Add; // button1
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
+            var u = new User()
+            {
+                FullName = textBox1.Text,
+            };
+            users.Add(u);
         }
     }
 }
